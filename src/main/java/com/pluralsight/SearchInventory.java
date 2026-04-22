a9da46b31f56b55dec52f7a6dc3b9bd5de514173
Nurbu <lamanurbu2@gmail.com>
2026-04-22 11:14:11 -0400

    Finished adding the products.

--- src/main/java/com/pluralsight/SearchInventory.java
+++ src/main/java/com/pluralsight/SearchInventory.java
@@ -1,4 +1,26 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class SearchInventory {
+    public static void main(String[] args) {
+        ArrayList<Product> inventory = getInventory();
+
+        for (Product product : inventory) {
+            System.out.println(product);
+        }
+
+    }
+
+    public static ArrayList<Product> getInventory() {
+        ArrayList<Product> inventory = new ArrayList<>();
+
+        inventory.add(new Product(1, "Watch", 503.4));
+        inventory.add(new Product(2, "Iphone", 1234.4));
+        inventory.add(new Product(3, "Gum", 2.4));
+        inventory.add(new Product(4, "Ipad", 231.4));
+        inventory.add(new Product(5, "Laptop", 423.4));
+
+        return inventory;
+    }
 }
